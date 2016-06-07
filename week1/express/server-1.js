8bfa1917478fb6a20258861702fcdcdf7a2e1278
DIALLO Ibrahima Sory <soryboums@gmail.com>
2016-06-07 13:50:04 +0200

    Add the gitignore file and express part

--- week1/express/server-1.js
+++ week1/express/server-1.js
@@ -1,12 +1,18 @@
-var http = require('http');
+var express = require('express'),
+    http = require('http');
+
 var hostname = 'localhost';
 var port = 3000;
 
-var server = http.createServer(function(req, res){
+var app = express();
+
+app.use(function(req, res, next){
     console.log(req.headers);
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('<h1>Hello World</h1>');
-})
+});
+
+var server = http.createServer(app);
 
 server.listen(port, hostname, function(){
     console.log('Server running at http://'+hostname+':'+port+'/');
